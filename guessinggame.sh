10b26d36b48f849e9a2fac46fd2f0e8d7ac793b4
pratik6217 <64962582+pratik6217@users.noreply.github.com>
2020-09-26 20:01:34 +0530

    Create guessinggame.sh

---
+++ guessinggame.sh
@@ -0,0 +1,20 @@
+
+#! /bin/bash
+condition=1
+l=$(ls  | wc -w)
+while [[ $condition -gt 0 ]]
+do
+        echo "Enter Your guess:"
+        read count
+        if [[ count -eq $l ]]
+        then
+                echo "Congracts You guessed it correctly!"
+                let condition=condition-1
+                break
+        elif [[ count -lt $l ]]
+        then
+                echo "Your guess is too low!"
+        else
+                echo "Your guess is too high!"
+        fi
+done
